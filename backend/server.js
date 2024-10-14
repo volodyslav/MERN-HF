5c46d1bac6d944bfbe034799de0d46aa071629de
Vladyslav <vladver1211@gmail.com>
2024-10-14 19:58:52 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,19 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import aiModels from "./routes/aimodel.route.js"
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }));}
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +26,14 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 // Routes for ai models
 app.use("/api/v1/models", aiModels);
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
